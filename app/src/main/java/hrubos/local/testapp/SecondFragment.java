4d892d08740e8678cfb16fa75fd49a3ac9923b16
Ondřej Hruboš <hruboson@gmail.com>
2021-01-14 08:10:34 +0100

    Added button support on SecondFragment
    Added button and how to handle the button presses.
    
    This change was made on SecondFragment.java

--- app/src/main/java/hrubos/local/testapp/SecondFragment.java
+++ app/src/main/java/hrubos/local/testapp/SecondFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import hrubos.local.testapp.R;
 
 // every fragment has its own class - same as view or screen - here we can add controller function (for example add button functions)
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -18,4 +19,21 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.second_fragment, container, false); // Inflate the layout for this fragment
 
     }
+
+    @Override // How to handle buttons - we implement View.OnClickListener to our fragments
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button: // this is id of our button
+                showToast("Button was pressed"); // what is supposed to happen
+                break;
+            // we can have multiple buttons here:
+            // case R.id.button2: ...
+            // case R.id.button3: ...
+        }
+    }
+
+    protected void showToast(String text){
+        Toast.makeText(this.getActivity(), text, Toast.LENGTH_LONG).show(); // if we want to pass activity to method, we call method getActivity()
+    }
+
 }
