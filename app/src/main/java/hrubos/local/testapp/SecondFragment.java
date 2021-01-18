9e227d2a372dcbfc2444cd90ae06786503a3202c
Ondřej Hruboš <hruboson@gmail.com>
2021-01-18 09:48:07 +0100

    Repaired button function
    Button on SecondFragment now works correctly.

--- app/src/main/java/hrubos/local/testapp/SecondFragment.java
+++ app/src/main/java/hrubos/local/testapp/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -13,11 +14,17 @@ import hrubos.local.testapp.R;
 // every fragment has its own class - same as view or screen - here we can add controller function (for example add button functions)
 public class SecondFragment extends Fragment implements View.OnClickListener{
 
+    public Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.second_fragment, container, false); // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.second_fragment, container, false); // Inflate the layout for this fragment
 
+        this.button = v.findViewById(R.id.button); // we find the button on our fragment
+        this.button.setOnClickListener(this); // and set him onClickListener
+
+        return v;
     }
 
     @Override // How to handle buttons - we implement View.OnClickListener to our fragments
@@ -26,14 +33,17 @@ public class SecondFragment extends Fragment implements View.OnClickListener{
             case R.id.button: // this is id of our button
                 showToast("Button was pressed"); // what is supposed to happen
                 break;
+            default:
+                break;
             // we can have multiple buttons here:
             // case R.id.button2: ...
             // case R.id.button3: ...
         }
     }
 
-    protected void showToast(String text){
-        Toast.makeText(this.getActivity(), text, Toast.LENGTH_LONG).show(); // if we want to pass activity to method, we call method getActivity()
+    public void showToast(String text){
+        Toast.makeText(this.getActivity(),text, Toast.LENGTH_LONG).show();  // if we want to pass activity to method, we call method getActivity()
+        return;
     }
 
 }
